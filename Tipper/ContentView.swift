521e46ca4ea6c910b71088ea17ec18d5ee3ddbc3
Marco Wolff <marcowolff619@gmail.com>
2020-08-05 14:23:59 -0400

    Initial Commit

---
+++ Tipper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tipper
+//
+//  Created by Mac Mini on 8/5/20.
+//  Copyright © 2020 Marco Wolff. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
